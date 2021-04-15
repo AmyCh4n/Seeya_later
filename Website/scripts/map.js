d5ff2547295953f031d9657c0bbbff26b384eea7
signesw <zcftssw@ucl.ac.uk>
2021-04-15 19:23:48 +0100

    added death data to MSOA layer display, changed border display based on zoom level

--- Website/scripts/map.js
+++ Website/scripts/map.js
@@ -3,11 +3,14 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiemNmdHNzdyIsImEiOiJja212c24wdW4wNmsxMm9tdG5ud
 var map = new mapboxgl.Map({
   container: 'mapbox', // container id
   style: 'mapbox://styles/zcftssw/cknagpjwh3dm617qxeokn7amw', // style URL
-  center: [-2, 50], // starting position [lng, lat]
-  zoom: 5.8 // starting zoom
+  center: [-1.434713, 52.622569], // starting position [lng, lat]
+  zoom: 5.4,// starting zoom
+  minZoom: 4.5// min zoom
 });
 //Adding a hoveredStateId
 var hoveredStateId = null;
+//Adding a clicked MSOA id
+var clickedMSOAId=null;
 // Add zoom and rotation controls to the map.
 map.addControl(new mapboxgl.NavigationControl());
 // Add a search bar to the map
@@ -37,26 +40,52 @@ map.on('load', function() {
     'id': 'msoa',
     'type': 'fill',
     'source': 'MSOAs', // reference the data source
-    'layout': {},
+    'layout': {'visibility':'none'},
     'paint': {
-      'fill-color': '#F27405', // orange color fill
+      'fill-color': [
+'interpolate',
+['linear'],
+['get', 'preventable_deaths'],25.90,
+'#F6DE7F',
+71.50,
+'#F29F05',
+86.10,
+'#F27405',
+104.70,
+'#F24405',
+132.20,
+'#F20505'], // orange color fill
       'fill-opacity': [
         'case',
         ['boolean', ['feature-state', 'hover'], false],
         1,
-        0.5 //Change opacity based on hover state
+        0.75 //Change opacity based on hover state
       ]
     }
   });
-  map.addLayer({
-    'id': 'outline',
-    'type': 'line',
-    'source': 'MSOAs',
-    'layout': {},
-    'paint': {
-      'line-color': '#ffecbd',
-      'line-width': 0.5
-    }
+
+map.addLayer({
+  'id': 'msoa-borders',
+  'type': 'line',
+  'source': 'MSOAs',
+  'layout':  {'visibility':'none'},
+  'paint': {
+  'line-color': '#66605b',
+  'line-width': [// change visibility of border based on zoom level
+      "interpolate", ["linear"], ["zoom"],
+      // zoom is 5 (or less) -> line will be invisible
+      6.4, ['case',
+        ['boolean', ['feature-state', 'clicked'], false],2,0],
+      //    2,//size will be 2 if clicked,
+      // zoom is 10 (or greater) -> line will be visible
+      8, ['case',['boolean', ['feature-state', 'clicked'], false],2,0.1]//Change size based on clicked state
+    ]
+//[
+//    'case',
+//    ['boolean', ['feature-state', 'clicked'], false],
+//    2,//size will be 2 if clicked
+//  }
+}
   });
 
   //Change colour on hover
@@ -82,7 +111,7 @@ map.on('load', function() {
 
   // When the mouse leaves the state-fill layer, update the feature state of the
   // previously hovered feature.
-  map.on('mouseleave', 'msoas', function() {
+  map.on('mouseleave', 'msoa', function() {
     if (hoveredStateId !== null) {
       map.setFeatureState({
         source: 'MSOAs',
@@ -94,4 +123,61 @@ map.on('load', function() {
     hoveredStateId = null;
   });
 
-}); 
+  // Display the name of the MSOA
+  map.on('mousemove', function(e) {
+    var states = map.queryRenderedFeatures(e.point, {
+      layers: ['msoa']
+    });
+
+    if (states.length > 0) {
+      document.getElementById('msoa_name').innerHTML = '<strong>MSOA: </strong>' + states[0].properties.msoa11hclnm + '';
+    } else {
+      document.getElementById('msoa_name').innerHTML = '<strong>MSOA:</strong>';
+    }
+  });
+
+map.on('click', 'msoa', (e) => {
+  map.getCanvas().style.cursor = 'pointer';
+  if (e.features.length > 0) {
+    if (clickedMSOAId) {
+      map.removeFeatureState({
+        source: "MSOAs",
+        id: clickedMSOAId
+      });
+    }
+
+    clickedMSOAId = e.features[0].id;
+
+    map.setFeatureState({
+      source: 'MSOAs',
+      id: clickedMSOAId,
+    }, {
+      clicked: true
+    });
+  }
+});
+});
+
+// Change visibility of layer depending on radio button toggle
+function DisplayLayer(){
+$('#Deaths').click(function(){
+        if($(this).is(':checked')){
+          // Hide the clusters layer (if it's not already hidden)
+          // Show the deaths layer
+          map.setLayoutProperty('msoa', 'visibility', 'visible');
+          map.setLayoutProperty('msoa-borders', 'visibility', 'visible');
+                }
+        else{
+          map.setLayoutProperty('msoas', 'visibility', 'none');
+        }
+      });
+$('#Clusters').click(function(){
+  if($(this).is(':checked')){
+    //Hide deaths layer
+    map.setLayoutProperty('msoa', 'visibility', 'none');
+    map.setLayoutProperty('msoa-borders', 'visibility', 'none')
+    //Show the culsters layer
+  }
+});
+};
+DisplayLayer();
