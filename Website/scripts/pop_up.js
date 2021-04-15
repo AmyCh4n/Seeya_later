a7d9023911cefad3e4b3497f938b63b23f27b5da
signesw <zcftssw@ucl.ac.uk>
2021-04-15 19:24:16 +0100

    cosmetics

--- Website/scripts/pop_up.js
+++ Website/scripts/pop_up.js
@@ -2,6 +2,7 @@
 $('input[type="radio"]').click(function() {
   if ($(this).is(':checked')) {
     var inputValue = $(this).attr("value");
+    console.log(inputValue);
     var targetBox = $("." + inputValue);
     $(targetBox).show();
     $(".box").not(targetBox).hide();
