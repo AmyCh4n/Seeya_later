08bc2049d501962c517d4fb499b175fde5c70797
Amy Ch4n <amychan_@live.co.uk>
2021-05-11 01:10:50 +0100

    All graphs added, need to edit age and ethnicities graphs next

--- Website/scripts/story.js
+++ Website/scripts/story.js
@@ -1,3 +1,19 @@
+/*collapsible menu*/
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for(i=0; i<coll.length; i++){
+  coll[i].addEventListener("click",function(){
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block"){
+      content.style.display = "none";
+    } else{
+      content.style.display = "block";
+    }
+  });
+}
+
 /*Preventable Deaths*/
 $.get('https://raw.githubusercontent.com/signesw/Seeya_later/main/Website/data/deaths.csv', function (data) {
 
@@ -8,7 +24,7 @@ var lengthlines=lines.length;
         data = lines.slice(1,lengthlines);
         console.log(data);
 
-Highcharts.chart('spdChart', {
+Highcharts.chart('spdChart',{
     title: {
         text: 'Distribution of Preventable Deaths'
     },
@@ -54,12 +70,12 @@ Highcharts.chart('spdChart', {
 /*Age*/
 $.get('https://raw.githubusercontent.com/signesw/Seeya_later/main/Website/data/age.csv', function (data) {
 
-        var lines = data.split('\n').map(function(aItem) {
-    return parseFloat(aItem);
+        var lines = data.split('\n').map(function(item) {
+    return parseFloat(item);
 });
-var lengthALines=lines.length;
-        aData = lines.slice(1,lengthALines);
-        console.log(aData);
+var lengthlines=lines.length;
+        data = lines.slice(1,lengthlines);
+        console.log(data);
 
 Highcharts.chart('sAgeChart', {
     title: {
@@ -67,10 +83,10 @@ Highcharts.chart('sAgeChart', {
     },
     xAxis: [{
         title: { text: '' },
-        opposite:true,
-    }, { title: { text: 'Age Ratio' },
-        opposite:false,
-
+        opposite: false
+    }, {
+        title: { text: 'Age Ratio' },
+        opposite: false
     }],
 
     yAxis: [{
@@ -81,25 +97,73 @@ Highcharts.chart('sAgeChart', {
     }],
 
     series: [{
-    showInLegend:false,
-        name: 'Count of MSOAs',
+        name: '0-15',
         type: 'histogram',
         xAxis: 1,
         yAxis: 1,
         baseSeries: 's1',
-        zIndex: 0,
-
+        zIndex: 0
     }, {
-        name: 'aData',
+        name: 'Data',
         visible: false,
         showInLegend: false,
        // type: 'scatter',
-        data: aData,
+        data: data,
         id: 's1',
         marker: {
             radius: 1.5
         }
-
+    },{
+        name: '16-24',
+        type: 'histogram',
+        xAxis: 1,
+        yAxis: 1,
+        baseSeries: 's2',
+        zIndex: 0
+    }, {
+        name: 'Data',
+        visible: false,
+        showInLegend: false,
+       // type: 'scatter',
+        data: data,
+        id: 's2',
+        marker: {
+            radius: 1.5
+        }
+    },{
+        name: '25-64',
+        type: 'histogram',
+        xAxis: 1,
+        yAxis: 1,
+        baseSeries: 's3',
+        zIndex: 0
+    }, {
+        name: 'Data',
+        visible: false,
+        showInLegend: false,
+       // type: 'scatter',
+        data: data,
+        id: 's3',
+        marker: {
+            radius: 1.5
+        }
+    },{
+        name: '64+',
+        type: 'histogram',
+        xAxis: 1,
+        yAxis: 1,
+        baseSeries: 's4',
+        zIndex: 0
+    }, {
+        name: 'Data',
+        visible: false,
+        showInLegend: false,
+       // type: 'scatter',
+        data: data,
+        id: 's4',
+        marker: {
+            radius: 1.5
+        }
     }]
 });
 })
@@ -153,6 +217,141 @@ Highcharts.chart('sImdChart', {
             radius: 1.5
         }
 
+    }]
+});
+})
+
+/*Ethnicity*/
+$.get('https://raw.githubusercontent.com/signesw/Seeya_later/main/Website/data/Ethnicities.csv', function (data) {
+
+        var lines = data.split('\n').map(function(item) {
+    return parseFloat(item);
+});
+var lengthlines=lines.length;
+        data = lines.slice(1,lengthlines);
+        console.log(data);
+
+Highcharts.chart('sEthnicityChart', {
+    title: {
+        text: 'Distribution of People by Ethnicity'
+    },
+    xAxis: [{
+        title: { text: '' },
+        opposite: false
+    }, {
+        title: { text: 'Ethnicity Ratio' },
+        opposite: false
+    }],
+
+    yAxis: [{
+        title: { text: '' }
+    }, {
+        title: { text: 'Count of MSOAs' },
+        opposite: false
+    }],
+
+    series: [{
+        name: 'White',
+        type: 'histogram',
+        xAxis: 1,
+        yAxis: 1,
+        baseSeries: 's1',
+        zIndex: 0
+    }, {
+        name: 'Data',
+        visible: false,
+        showInLegend: false,
+       // type: 'scatter',
+        data: data,
+        id: 's1',
+        marker: {
+            radius: 1.5
+        }
+    },{
+        name: 'Indian',
+        type: 'histogram',
+        xAxis: 1,
+        yAxis: 1,
+        baseSeries: 's2',
+        zIndex: 0
+    }, {
+        name: 'Data',
+        visible: false,
+        showInLegend: false,
+       // type: 'scatter',
+        data: data,
+        id: 's2',
+        marker: {
+            radius: 1.5
+        }
+    },{
+        name: 'Pakistani',
+        type: 'histogram',
+        xAxis: 1,
+        yAxis: 1,
+        baseSeries: 's3',
+        zIndex: 0
+    }, {
+        name: 'Data',
+        visible: false,
+        showInLegend: false,
+       // type: 'scatter',
+        data: data,
+        id: 's3',
+        marker: {
+            radius: 1.5
+        }
+    },{
+        name: 'Bangladeshi',
+        type: 'histogram',
+        xAxis: 1,
+        yAxis: 1,
+        baseSeries: 's4',
+        zIndex: 0
+    }, {
+        name: 'Data',
+        visible: false,
+        showInLegend: false,
+       // type: 'scatter',
+        data: data,
+        id: 's4',
+        marker: {
+            radius: 1.5
+        }
+    },{
+        name: 'Chinese',
+        type: 'histogram',
+        xAxis: 1,
+        yAxis: 1,
+        baseSeries: 's4',
+        zIndex: 0
+    }, {
+        name: 'Data',
+        visible: false,
+        showInLegend: false,
+       // type: 'scatter',
+        data: data,
+        id: 's4',
+        marker: {
+            radius: 1.5
+        }
+    },{
+        name: 'Black',
+        type: 'histogram',
+        xAxis: 1,
+        yAxis: 1,
+        baseSeries: 's4',
+        zIndex: 0
+    }, {
+        name: 'Data',
+        visible: false,
+        showInLegend: false,
+       // type: 'scatter',
+        data: data,
+        id: 's4',
+        marker: {
+            radius: 1.5
+        }
     }]
 });
 })
